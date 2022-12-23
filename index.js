c7832777f3f3936b8ff1651edaee9ccad0148f16
Mohamed Hossam <81392900+mohamedhoss123@users.noreply.github.com>
2022-12-23 13:58:08 +0200

    change cli version

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const program = new Command();
 program
   .name("exp-cli")
   .description("CLI tool to make your express project more fun")
-  .version("0.1.0");
+  .version("0.2.1");
 
 program
   .command("new")
